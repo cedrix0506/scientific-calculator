621785774d169f5fe1802e221e991cd4b30ab745
cedrix0506 <cedrix0506@gmail.com>
2026-07-15 13:59:18 -0600

    feat: create calculator v0.1 with addition

---
+++ main.py
@@ -0,0 +1,9 @@
+print("=" * 30)
+print("     CALCULATOR")
+print("=" * 30)
+name = input("Enter your name: ")
+print(f"Hello, {name}! Welcome to the calculator.")
+number1 = float(input ("Enter the first number :"))
+number2 = float(input ("Enter the second number :"))
+result = number1 + number2
+print(f"The sum of {number1} and {number2} is {number1 + number2}")
